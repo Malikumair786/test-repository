4937988c9d99fefabd1e51202d479e933769205a
Your Name <you@example.com>
2023-11-01 18:29:12 +0500

    commit 2 again again

--- test/src/App.js
+++ test/src/App.js
@@ -16,16 +16,6 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-<<<<<<< HEAD
-          commit 2
-          commit 3
-          commit 4
-          commit 5
-          commit 6
-          commit 7
-          commit 8
-=======
->>>>>>> parent of 5d41673 (commit 2)
         </a>
       </header>
     </div>
